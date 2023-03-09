689493a779b516f71103210dd5cdc479d10c0a35
bellDev-code <sumaoo20@naver.com>
2023-03-09 15:57:24 +0900

    Styled-Components & props pratice

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,10 +1,33 @@
+import { useState } from "react";
+import styled, { css } from "styled-components";
 import { isLoggedInVar } from "../apollo";
 
+const Title = styled.h1`
+  color: ${(props) => (props.potato ? "palevioletred" : "beige")};
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  ${(props) =>
+    props.potato
+      ? css`
+          font-size: 49px;
+        `
+      : css`
+          text-decoration: underline;
+        `}
+`;
+
+const ToggleColor = styled.button`
+  color: red;
+`;
+
 export const Login = () => {
+  const [potato, setPotato] = useState(false);
+  const togglePotato = () => setPotato((current) => !current);
   return (
     <div>
-      <h1>Login</h1>
+      <Title potato={potato}>Login</Title>
       <button onClick={() => isLoggedInVar(true)}>Login Now!!</button>
+      <ToggleColor onClick={togglePotato}>Toggle Potato</ToggleColor>
     </div>
   );
 };
