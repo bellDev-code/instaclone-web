3ed607eb65e97b28929ff1111a8f0f6b73c89cfa
bellDev-code <sumaoo20@naver.com>
2023-03-20 13:47:31 +0900

    SignUp page

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -66,7 +66,7 @@ export const Login = () => {
     onCompleted,
   });
 
-  const onSubmitValid = (data) => {
+  const onSubmitValid = () => {
     if (loading) {
       return;
     }
--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,11 +1,15 @@
+import { gql, useMutation } from "@apollo/client";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { useHistory } from "react-router-dom";
 import styled from "styled-components";
 import AuthLayout from "../components/auth/AuthLayout";
 import BottomBox from "../components/auth/BottomBox";
 import Button from "../components/auth/Button";
 import FormBox from "../components/auth/FormBox";
+import FormError from "../components/auth/FormError";
 import Input from "../components/auth/Input";
 import { FatLink } from "../components/shared";
 import routes from "../routes";
@@ -22,7 +26,57 @@ const Subtitle = styled(FatLink)`
   margin-top: 10px;
 `;
 
+const CREATE_ACCOUNT_MUTATION = gql`
+  mutation createAccount(
+    $firstName: String!
+    $lastName: String
+    $username: String!
+    $email: String!
+    $password: String!
+  ) {
+    createAccount(
+      firstName: $firstName
+      lastName: $lastName
+      username: $username
+      email: $email
+      password: $password
+    ) {
+      ok
+      error
+    }
+  }
+`;
+
 export const SignUp = () => {
+  const history = useHistory();
+  const onCompleted = (data) => {
+    const {
+      createAccount: { ok, error },
+    } = data;
+
+    if (!ok) {
+      return;
+    }
+    history.push(routes.home);
+  };
+  const [createAccount, { loading }] = useMutation(CREATE_ACCOUNT_MUTATION, {
+    onCompleted,
+  });
+  const { register, handleSubmit, formState } = useForm({
+    mode: "onChange",
+  });
+
+  const onSubmitValid = (data) => {
+    if (loading) {
+      return;
+    }
+    createAccount({
+      variables: {
+        ...data,
+      },
+    });
+    console.log(data);
+  };
   return (
     <AuthLayout>
       <Helmet>
@@ -35,16 +89,59 @@ export const SignUp = () => {
             Sign up to see photos and videos from your friends.
           </Subtitle>
         </HeaderContainer>
-        <form>
-          <Input type="text" placeholder="Name" />
-          <Input type="text" placeholder="Email" />
-          <Input type="text" placeholder="Username" />
-          <Input type="password" placeholder="Password" />
-          <Button type="submit" value="Sign up" />
+        <form onSubmit={handleSubmit(onSubmitValid)}>
+          <Input
+            {...register("firstName", {
+              required: "firstName은 필수 입력 사항입니다.",
+            })}
+            type="text"
+            name="firstName"
+            placeholder="First Name"
+          />
+          <FormError message={formState.errors?.firstName?.message} />
+          <Input
+            {...register("lastName")}
+            type="text"
+            name="lastName"
+            placeholder="Last Name"
+          />
+          <Input
+            {...register("username", {
+              required: "username은 필수 입력 사항입니다.",
+            })}
+            type="text"
+            name="username"
+            placeholder="Username"
+          />
+          <FormError message={formState.errors?.username?.message} />
+          <Input
+            {...register("email", {
+              required: "email은 필수 입력 사항입니다.",
+            })}
+            type="text"
+            name="email"
+            placeholder="Email"
+          />
+          <FormError message={formState.errors?.email?.message} />
+          <Input
+            {...register("password", {
+              required: "password는 필수 입력 사항입니다.",
+            })}
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
+          <FormError message={formState.errors?.password?.message} />
+          <Button
+            type="submit"
+            value={loading ? "Loading..." : "Sign Up"}
+            disabled={!formState.isValid || loading}
+          />
         </form>
       </FormBox>
       <BottomBox cta="Have an account?" linkText="Log in" link={routes.home} />
     </AuthLayout>
   );
 };
+
 export default SignUp;
